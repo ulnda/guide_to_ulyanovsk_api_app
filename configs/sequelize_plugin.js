2d9796e55869aaaa8995bf53d895a515c730c736
d.nagibin <d.nagibin@fun-box.ru>
2016-02-11 17:06:11 +0300

    Host for Heroku.

--- configs/sequelize_plugin.js
+++ configs/sequelize_plugin.js
@@ -1,6 +1,7 @@
 module.exports = {
   register: require('hapi-sequelize'),
   options: {
+    host: 'ec2-54-83-17-9.compute-1.amazonaws.com',
     database: 'd7bdb4nkrk6nh1',
     user: 'vyrhgvwtnegxsn',
     pass: 'OSyYQYDGag4bNuHe0NN9SCzkBH',
