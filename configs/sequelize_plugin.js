77e067a0ae578292f7aa0f42f56b461a1a4e1dad
d.nagibin <d.nagibin@fun-box.ru>
2016-02-11 17:03:32 +0300

    Database, use, pass for Heroku.

--- configs/sequelize_plugin.js
+++ configs/sequelize_plugin.js
@@ -1,9 +1,9 @@
 module.exports = {
   register: require('hapi-sequelize'),
   options: {
-    database: 'guide_to_ulyanovsk',
-    user: 'guide_to_ulyanovsk',
-    pass: 'guide123',
+    database: 'd7bdb4nkrk6nh1',
+    user: 'vyrhgvwtnegxsn',
+    pass: 'OSyYQYDGag4bNuHe0NN9SCzkBH',
     dialect: 'postgres',
     models: ['./models/*.js'],
     port: 5432
